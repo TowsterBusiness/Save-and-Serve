c0ce955c6dc6d7a10fa54883a01425c9eab7c3b6
DaCodingBeast <146308828+DaCodingBeast@users.noreply.github.com>
2025-10-06 23:14:54 -0700

    New dummy api calls

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,5 @@
+from venv import create
+import PIL
 from fastapi import FastAPI
 import uvicorn
 from CompletedRequests import recipieMaker
@@ -16,3 +18,26 @@ async def getRecipies(image: bytes):
     worker.getRecipies(image)
     outputJSON = worker.outputJSON
     return {"response": outputJSON}
+
+
+@app.post("/getResponseJSON")
+async def getResponseJSON():
+    with open("ExampleOutput.json", "r") as f:
+        outputJSON = f.read()
+    return {"response": outputJSON}
+
+@app.post("/sendImageBinary")
+async def sendImageBinary(image: bytes):
+    #create a PIL image and check if succesfully made, return boolean
+    try:
+        img = PIL.Image.open(image)
+        worked = True
+    except:
+        worked = False
+    
+    return {"response": worked}
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
